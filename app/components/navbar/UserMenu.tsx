d8428ff915bcfc8016f516286410d269e440a48b
Tong Lin <wowawawoa@gmail.com>
2024-02-02 11:28:09 +1030

    feat: adding google and github auth

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -38,7 +38,7 @@ const UserMenu: React.FC<UserMenuProps> = ({ currentUser }) => {
         >
           <AiOutlineMenu />
           <div className="hidden md:block">
-            <Avatar src={undefined} />
+            <Avatar src={currentUser?.image} />
           </div>
         </div>
       </div>
