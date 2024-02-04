44a7f8941ab291e9e1566a7aafa4edb3e750c3eb
Tong Lin <wowawawoa@gmail.com>
2024-02-04 17:07:54 +1030

    feat: adding favorit function backend

--- app/components/HeartButton.tsx
+++ app/components/HeartButton.tsx
@@ -1,7 +1,9 @@
 "use client";
 
 import { AiFillHeart, AiOutlineHeart } from "react-icons/ai";
+
 import { SafeUser } from "../types";
+import useFavorite from "../hooks/useFavorite";
 
 interface HeartButtonProps {
   listingId: string;
@@ -12,8 +14,7 @@ const HeartButton: React.FC<HeartButtonProps> = ({
   listingId,
   currentUser,
 }) => {
-  const hasFavorite = false;
-  const toggleFavorite = () => {};
+  const {hasFavorited, toggleFavorite} = useFavorite({listingId, currentUser});
 
   return (
     <div
@@ -26,7 +27,7 @@ const HeartButton: React.FC<HeartButtonProps> = ({
       />
       <AiFillHeart
         size={24}
-        className={hasFavorite ? "fill-rose-500" : "fill-neutral-500/70"}
+        className={hasFavorited ? "fill-rose-500" : "fill-neutral-500/70"}
       />
     </div>
   );
