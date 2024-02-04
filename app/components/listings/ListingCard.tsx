8bd21af4f3c7072a87c4421c0cc30fd01fd8f184
Tong Lin <wowawawoa@gmail.com>
2024-02-04 16:24:25 +1030

    feat: adding listing card frontend, adding fetch listing backend

---
+++ app/components/listings/ListingCard.tsx
@@ -0,0 +1,84 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import { format } from "date-fns";
+import React, { useCallback, useMemo } from "react";
+import Image from "next/image";
+
+import { SafeUser } from "@/app/types";
+import { Listing, Reservation } from "@prisma/client";
+import useCountries from "@/app/hooks/useCountries";
+import HeartButton from "@/app/components/HeartButton";
+
+interface ListingCardProps {
+  data: Listing;
+  currentUser?: SafeUser | null;
+  onAction?: (id: string) => void;
+  disabled?: boolean;
+  actionId?: string;
+  reservation?: Reservation;
+  actionLabel?: string;
+}
+
+const ListingCard: React.FC<ListingCardProps> = ({
+  data,
+  currentUser,
+  reservation,
+}) => {
+  const router = useRouter();
+  const { getByValue } = useCountries();
+
+  const location = getByValue(data.locationValue);
+
+  const price = useMemo(() => {
+    if (reservation) {
+      return reservation.totalPrice;
+    }
+
+    return data.price;
+  }, [reservation, data.price]);
+
+  const reservationDate = useMemo(() => {
+    if (!reservation) {
+      return null;
+    }
+
+    const start = new Date(reservation.startDate);
+    const end = new Date(reservation.endDate);
+
+    return `${format(start, "PP")} - ${format(end, "PP")}`;
+  }, [reservation]);
+
+  return (
+    <div
+      onClick={() => router.push(`/listings/${data.id}`)}
+      className="col-span-1 cursor-pointer group"
+    >
+      <div className="flex flex-col gap-2 w-full">
+        <div className="aspect-square w-full relative overflow-hidden rounded-xl">
+          <Image
+            alt="Listing"
+            src={data.imageSrc}
+            fill
+            className="object-cover h-full w-full group-hover:scale-110 transition"
+          />
+          <div className="absolute top-3 right-3">
+            <HeartButton listingId={data.id} currentUser={currentUser} />
+          </div>
+        </div>
+        <div className="font-semibold text-lg">
+          {location?.region}, {location?.label}
+        </div>
+        <div className="font-light text-neutral-500">
+          {reservationDate || data.category}
+        </div>
+        <div className="flex flex-row items-center gap-1">
+          <div className="font-semibold">$ {price}</div>
+          {!reservation && <div className="font-light">night</div>}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ListingCard;
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,37 @@
+import getListings from "./actions/getListings";
+import ClientOnly from "./components/ClientOnly";
+import Container from "./components/Container";
+import EmptyState from "./components/EmptyState";
+import ListingCard from "./components/listings/ListingCard";
+import getCurrentUser from "./actions/getCurrentUser";
 
-export default function Home() {
+export default async function Home() {
+  const listings = await getListings();
+  const currentUser = await getCurrentUser();
+
+  if (listings.length === 0) {
+    return (
+      <ClientOnly>
+        <EmptyState showReset />
+      </ClientOnly>
+    );
+  }
+
   return (
-    <div>Hello Airbnb</div>
-  )
+    <ClientOnly>
+      <Container>
+        <div className="pt-24 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
+          {listings.map((listing: any) => {
+            return (
+              <ListingCard
+                key={listing.id}
+                data={listing}
+                currentUser={currentUser}
+              />
+            );
+          })}
+        </div>
+      </Container>
+    </ClientOnly>
+  );
 }
