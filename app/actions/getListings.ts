739385e94d9666417f40a68548e1029c5eb70a69
Tong Lin <wowawawoa@gmail.com>
2024-02-06 22:05:42 +1030

    feat:adding search function, adding loading page

--- app/actions/getListings.ts
+++ app/actions/getListings.ts
@@ -2,24 +2,84 @@ import prisma from "@/app/libs/prismadb";
 
 export interface ListingsParams {
   userId?: string;
+  guestCount?: number;
+  roomCount?: number;
+  bathroomCount?: number;
+  startDate?: string;
+  endDate?: string;
+  locationValue?: string;
+  category?: string;
 }
 
 export default async function getListings(params: ListingsParams) {
   try {
-    const { userId } = params;
+    const {
+      userId,
+      roomCount, 
+      guestCount, 
+      bathroomCount, 
+      locationValue,
+      startDate,
+      endDate,
+      category,
+    } = params;
 
-    // if query is empty, it will return all listings
     let query: any = {};
 
     if (userId) {
       query.userId = userId;
     }
 
+    if (category) {
+      query.category = category;
+    }
+
+    if (roomCount) {
+      query.roomCount = {
+        gte: +roomCount
+      }
+    }
+
+    if (guestCount) {
+      query.guestCount = {
+        gte: +guestCount
+      }
+    }
+
+    if (bathroomCount) {
+      query.bathroomCount = {
+        gte: +bathroomCount
+      }
+    }
+
+    if (locationValue) {
+      query.locationValue = locationValue;
+    }
+
+    if (startDate && endDate) {
+      query.NOT = {
+        reservations: {
+          some: {
+            OR: [
+              {
+                endDate: { gte: startDate },
+                startDate: { lte: startDate }
+              },
+              {
+                startDate: { lte: endDate },
+                endDate: { gte: endDate }
+              }
+            ]
+          }
+        }
+      }
+    }
+
     const listings = await prisma.listing.findMany({
       where: query,
       orderBy: {
-        createdAt: "desc",
-      },
+        createdAt: 'desc'
+      }
     });
 
     const safeListings = listings.map((listing) => ({
--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,7 @@ import LoginModal from "./components/modals/LoginModal";
 import RentModal from "./components/modals/RentModal";
 import ToasterProvider from "./providers/ToasterProvider";
 import getCurrentUser from "./actions/getCurrentUser";
+import SearchModal from "./components/modals/searchModal";
 
 export const metadata = {
   title: "My Airbnb",
@@ -30,6 +31,7 @@ export default async function RootLayout({
       <body className={font.className}>
         <ClientOnly>
           <ToasterProvider />
+          <SearchModal />
           <RentModal />
           <RegisterModal />
           <LoginModal />
